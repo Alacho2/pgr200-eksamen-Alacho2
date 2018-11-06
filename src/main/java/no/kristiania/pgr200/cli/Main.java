f8b4f2c54bd6b51dbeb6287d24f693f88901049c
Alexander <breale17@egms.no>
2018-11-06 15:26:01 +0100

    .

--- src/main/java/no/kristiania/pgr200/cli/Main.java
+++ src/main/java/no/kristiania/pgr200/cli/Main.java
@@ -13,7 +13,7 @@ import java.util.Arrays;
 public class Main {
   private static HttpServerListener server;
 
-  public static void main(String[] args) throws IOException, URISyntaxException {
+  public static void main(String[] args) throws IOException {
     new DbConfig();
     ParseCommands.parseAllCommands();
     startServer();
