0968f625c51ab1f3dbde60ac0254b7fdc2179eac
Jason Williams <jlwcrews@gmail.com>
2018-11-06 11:41:34 +0100

    @FixMethodOrder implemented in HttpServerCapiTest

--- src/test/java/no/kristiania/pgr200/server/HttpServerCapiTest.java
+++ src/test/java/no/kristiania/pgr200/server/HttpServerCapiTest.java
@@ -1,15 +1,21 @@
 package no.kristiania.pgr200.server;
 
 import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
 import java.io.IOException;
 import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+
 public class HttpServerCapiTest {
+
+
 
     private static HttpServerListener server;
     int port = 0;
@@ -27,9 +33,10 @@ public class HttpServerCapiTest {
         server.start(0);
     }
 
+
+
     @Test
-    @Ignore
-    public void shouldParseJsonConferenceInsert() throws IOException{
+     public void test1shouldParseJsonConferenceInsert() throws IOException{
         HttpClientRequest request = new HttpClientRequest("localhost", server.getPort(), "/capi", "POST", "application/json",
                 "{\"mode\":\"insert\",\"table\":\"conference\",\"fields\":[{\"name\":\"title\",\"value\":\"myTitle\"},{\"name\":\"description\"," +
                         "\"value\":\"myDescription\"},{\"name\":\"time-start\",\"value\":\"10-10-2010\"},{\"name\":\"time-end\",\"value\":\"10-10-2018\"}]}");
@@ -39,8 +46,7 @@ public class HttpServerCapiTest {
     }
 
     @Test
-    @Ignore
-    public void shouldParseJsonTrackInsert() throws IOException{
+    public void test2shouldParseJsonTrackInsert() throws IOException{
         HttpClientRequest request = new HttpClientRequest("localhost", server.getPort(), "/capi", "POST", "application/json",
                 "{\"mode\":\"insert\",\"table\":\"track\",\"fields\":[{\"name\":\"title\",\"value\":\"Track 5\"},{\"name\":\"description\"," +
                         "\"value\":\"myDescription\"},{\"name\":\"track_conference_id\",\"value\":\"1\"}]}");
@@ -50,8 +56,7 @@ public class HttpServerCapiTest {
     }
 
     @Test
-    @Ignore
-    public void shouldParseJsonTalkInsert() throws IOException{
+    public void test3shouldParseJsonTalkInsert() throws IOException{
         HttpClientRequest request = new HttpClientRequest("localhost", server.getPort(), "/capi", "POST", "application/json",
                 "{\"mode\":\"insert\",\"table\":\"talk\",\"fields\":[{\"name\":\"title\",\"value\":\"talk title\"},{\"name\":\"description\"," +
                         "\"value\":\"myDescription\"},{\"name\":\"time-start\",\"value\":\"10-10-2010\"},{\"name\":\"time-end\",\"value\":\"10-10-2018\"}]}");
@@ -59,10 +64,9 @@ public class HttpServerCapiTest {
     }
 
     @Test
-    @Ignore
-    public void shouldParseCapiPathConferenceRetrieve() throws IOException{
+    public void test4shouldParseCapiPathConferenceRetrieve() throws IOException{
         HttpClientRequest request = new HttpClientRequest("localhost", server.getPort(), "/capi", "POST","application/json",
-                "{\"mode\":\"retrieve\",\"table\":\"conference\",\"fields\":[{\"name\":\"id\",\"value\":7}]}");
+                "{\"mode\":\"retrieve\",\"table\":\"conference\",\"fields\":[{\"name\":\"id\",\"value\":1}]}");
         HttpClientResponse response = request.execute();
 
         assertThat(response.getBody()).contains("\"date_start\":\"2010-10-10\",\"date_end\":\"2018-10-10\",\"title\":\"myTitle\",\"description\":\"myDescription\"");
@@ -70,19 +74,18 @@ public class HttpServerCapiTest {
     }
 
     @Test
-    @Ignore
-    public void shouldParseCapiPathTrackRetrieve() throws IOException{
+    public void test5shouldParseCapiPathTrackRetrieve() throws IOException{
         HttpClientRequest request = new HttpClientRequest("localhost", server.getPort(), "/capi", "POST","application/json",
-                "{\"mode\":\"retrieve\",\"table\":\"track\",\"fields\":[{\"name\":\"id\",\"value\":5}]}");
+                "{\"mode\":\"retrieve\",\"table\":\"track\",\"fields\":[{\"name\":\"id\",\"value\":1}]}");
         HttpClientResponse response = request.execute();
-        System.out.println(response.getBody());
 
+        assertThat(response.getBody()).contains("\"track_conference_id\":1,\"title\":\"Track 5\",\"description\":\"myDescription\"");
         assertThat(response.getStatusCode()).isEqualTo(200);
     }
 
     @Test
     @Ignore
-    public void shouldParseCapiPathTalkRetrieve() throws IOException {
+    public void test6shouldParseCapiPathTalkRetrieve() throws IOException {
         HttpClientRequest request = new HttpClientRequest("localhost", server.getPort(), "/capi/talk", "POST","application/json",
                 "{\"mode\":\"retrieve\",\"table\":\"talk\",\"fields\":[{\"name\":\"id\",\"value\":1}]}");
         HttpClientResponse response = request.execute();
@@ -92,7 +95,7 @@ public class HttpServerCapiTest {
 
     @Test
     @Ignore
-    public void shouldFailParseCapiPathTalkRetrieve() throws IOException {
+    public void test7shouldFailParseCapiPathTalkRetrieve() throws IOException {
         HttpClientRequest request = new HttpClientRequest("localhost", server.getPort(), "/capi/talk", "POST", "application/json",
                 "");
         HttpClientResponse response = request.execute();
@@ -102,7 +105,7 @@ public class HttpServerCapiTest {
 
     @Test
     @Ignore
-    public void shouldFailCapiInvalidTable() throws IOException {
+    public void test8shouldFailCapiInvalidTable() throws IOException {
         HttpClientRequest request = new HttpClientRequest("localhost", server.getPort(), "/capi/talk", "POST","application/json",
                 "{\"mode\":\"retrieve\",\"table\":\"failtable\",\"fields\":[{\"name\":\"id\",\"value\":1}]}");
         HttpClientResponse response = request.execute();
@@ -112,7 +115,7 @@ public class HttpServerCapiTest {
 
     @Test
     @Ignore
-    public void shouldParseJsonConferenceDelete() throws IOException{
+    public void test9shouldParseJsonConferenceDelete() throws IOException{
         HttpClientRequest request = new HttpClientRequest("localhost", server.getPort(), "/capi/conference", "POST","application/json",
                 "{\"mode\":\"delete\",\"table\":\"conference\",\"fields\":[{\"name\":\"id\",\"value\":5}]}");
         HttpClientResponse response = request.execute();
@@ -122,7 +125,7 @@ public class HttpServerCapiTest {
 
     @Test
     @Ignore
-    public void shouldParseJsonReset() throws IOException{
+    public void test10shouldParseJsonReset() throws IOException{
         HttpClientRequest request = new HttpClientRequest("localhost", server.getPort(), "/capi/reset", "POST", "application/json",
                 "{\"mode\":\"reset\"}");
         HttpClientResponse response = request.execute();
