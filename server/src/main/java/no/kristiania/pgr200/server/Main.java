f3342ee3fce2df55c33d011eb73a720505f3d4a0
Jason Williams <jlwcrews@gmail.com>
2018-11-11 12:09:36 +0100

    fixed server main to now blow up without args

--- server/src/main/java/no/kristiania/pgr200/server/Main.java
+++ server/src/main/java/no/kristiania/pgr200/server/Main.java
@@ -1,6 +1,5 @@
 package no.kristiania.pgr200.server;
 
-import no.kristiania.pgr200.common.PropertyReader;
 import no.kristiania.pgr200.db.LocalDataSource;
 import no.kristiania.pgr200.server.requesthandlers.HttpServerRequestHandlerBadHttpMethod;
 import no.kristiania.pgr200.server.requesthandlers.HttpServerRequestHandlerCapi;
@@ -13,16 +12,8 @@ import java.util.Arrays;
 
 public class Main {
 
-    private static int PORT;
-    private static String HOST_NAME;
-    private static String WEB_ROOT;
-    private static String DEFAULT_FILE;
-    private static String SERVER_NAME;
-    private static String FILE_NOT_FOUND;
-    private static String METHOD_NOT_SUPPORTED;
-
     public static void main(String[] args) throws IOException {
-        if(args[0] != null && args[0].toLowerCase().equals("reset")){
+        if(args.length > 0 && args[0].toLowerCase().equals("reset")){
             DataSource dataSource = new LocalDataSource().resetDataSource();
             System.out.println("Database has been reset.");
             System.exit(0);
